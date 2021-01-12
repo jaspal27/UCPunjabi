e68e6c6ec38b7fa4a635d850c761729229d95559
Jaspal Singh <jaspal27@gmail.com>
2021-01-11 20:05:11 -0800

    fixed the code

--- src/nav/MainNav.tsx
+++ src/nav/MainNav.tsx
@@ -1,17 +1,16 @@
 import 'react-native-gesture-handler';
-import React, { createRef, memo, useCallback, useEffect, useRef } from 'react';
-import { NavigationContainer, navigationRef, Navigator, ROUTERS, Screen } from "utils/navigation";
-import { StackNavigationOptions } from "@react-navigation/stack";
+import React, {  memo, useCallback, useEffect, useRef } from 'react';
+import { NavigationContainer, navigationRef, Navigator, ROUTERS, Screen } from 'utils/navigation'
 import SplashScreen from 'react-native-splash-screen'
 // @ts-ignore
 
-import Walkthroughs from "screens/Walkthroughs";
+
 import Home from "screens/Home";
 import Gurmukhi from "screens/Gurmukhi";
 import GurmukhiDetails from "screens/Gurmukhi/Details"
 import Gurmukhi2ndScreen from "screens/Gurmukhi/Gurmukhi2nd"
 import WordFormation from "screens/WordFormation"
-import { Platform } from "react-native";
+
 import WordFormationDetails from 'screens/WordFormation/Details';
 
 const optionNavigator: any = {
